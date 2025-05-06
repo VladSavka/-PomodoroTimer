b724fb06de92a7d57bbc718b0c120566b5f29c31
Vlad Savka <vlad.savka@august.com>
2025-05-06 11:51:51 +0200

    bug fix mewing timer

--- composeApp/src/wasmJsMain/kotlin/org/timer/main/timer/AlarmPlayer.kt
+++ composeApp/src/wasmJsMain/kotlin/org/timer/main/timer/AlarmPlayer.kt
@@ -1,19 +1,32 @@
 package org.timer.main.timer
 
-import org.w3c.dom.Audio
+import org.jetbrains.compose.resources.*
+import org.w3c.dom.*
+import pomodorotimer.composeapp.generated.resources.*
 
+@OptIn(ExperimentalResourceApi::class)
 actual class AlarmPlayer actual constructor(actual val context: Any?) {
-    private val audio =
-        Audio("https://upload.wikimedia.org/wikipedia/commons/transcoded/5/53/Felis_silvestris_catus_meows.ogg/Felis_silvestris_catus_meows.ogg.mp3?download")
+    private val audio: Audio
 
+
+    init {
+        val url = Res.getUri("files/Felis_silvestris_catus_meows.ogg.mp3")
+        audio = Audio(url)
+    }
 
     actual fun play(onEnded: () -> Unit) {
-        audio.play()
-        audio.onended = {
-            audio.removeEventListener("ended", audio.onended)
-            audio.currentTime = 0.0
+        try {
+            audio.play()
+            audio.onended = {
+                audio.removeEventListener("ended", audio.onended)
+                audio.currentTime = 0.0
+                onEnded.invoke()
+            }
+        } catch (e: Exception) {
+            print("Error playing audio: " + e.message)
             onEnded.invoke()
         }
+
     }
 
     actual fun cancel() {
